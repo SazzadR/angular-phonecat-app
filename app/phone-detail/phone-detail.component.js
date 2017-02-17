008a80a452cc2b4ab387127d84d038d9ed7a77f3
Sazzadur Rahman <sazzad.rahman.bd@gmail.com>
2017-02-17 22:32:00 +0600

    More Templating

--- app/phone-detail/phone-detail.component.js
+++ app/phone-detail/phone-detail.component.js
@@ -1,10 +1,14 @@
 angular.
     module('phoneDetail').
         component('phoneDetail', {
-            template: 'TBD: Detail view for <span>{{ $ctrl.phoneId }}</span>',
-            controller: ['$routeParams', '$log',
-                function PhoneListController($routeParams, $log) {
-                    this.phoneId = $routeParams.phoneId
+            templateUrl: 'phone-detail/phone-detail.template.html',
+            controller: ['$routeParams', '$http', '$log',
+                function PhoneListController($routeParams, $http, $log) {
+                    var self = this;
+
+                    $http.get('phones/' + $routeParams.phoneId + '.json').then(function (response) {
+                        self.phone = response.data;
+                    });
                 }
             ]
         });
